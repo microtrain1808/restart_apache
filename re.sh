6f8d917fece776952babc3453776396c7d30d95e
Jason Snider <root@jasonsnider.com>
2018-08-14 14:03:52 -0500

    Added the ability to specify vhosts and systemctl commands

--- re.sh
+++ re.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+CONFIG="$1"
+COMMAND="$2"
+
+if [ "$#" -ne 2 ]
+then
+    echo "$0 requires two parameters {virtual-host} {restart|reload}"
+    exit 1
+fi
+
 cd /etc/apache2/sites-available
 
 # Disable VHOSTS
-sudo a2dissite *
-sudo systemctl restart apache2
+sudo a2dissite "$CONFIG"
+sudo systemctl "$COMMAND" apache2
 
 # Enable VHOSTS
-sudo a2ensite *
-sudo systemctl restart apache2
+sudo a2ensite "$CONFIG"
+sudo systemctl "$COMMAND" apache2
